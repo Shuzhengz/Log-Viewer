81d63dfef3f5b9989ef585c7f33a0f0237c03c41
Yi Zhang <artificialstyrofoam@gmail.com>
2021-02-14 16:18:07 -0800

    processing

--- src/main/java/com/team1678/logviewer/Main.java
+++ src/main/java/com/team1678/logviewer/Main.java
@@ -1,15 +1,23 @@
 package com.team1678.logviewer;
 
+import com.opencsv.exceptions.CsvException;
+import com.team1678.logviewer.backend.Processor;
 import com.team1678.logviewer.frontend.Application;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
 
+    private static Processor processor = new Processor();
     /**
      * Main method for the whole application.
      * @param args unused command-line arguments
      */
 
-    public static void main(String[] args) {
-        Application.app.mainloop();
+
+    public static void main(String[] args) throws IOException, CsvException {
+        //Application.app.mainloop();
+
     }
 }
--- src/main/java/com/team1678/logviewer/backend/Processor.java
+++ src/main/java/com/team1678/logviewer/backend/Processor.java
@@ -1,4 +1,22 @@
 package com.team1678.logviewer.backend;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Processor {
+
+    public static double[][] process(double[] timestamps, double[] data) {
+        double[][] timeVsData = new double[timestamps.length][2];
+        for (int i=0;i<=timestamps.length-1;i++) {
+            timeVsData[i][0] = timestamps[i];
+            timeVsData[i][1] = data[i];
+        }
+        return timeVsData;
+    }
+
 }

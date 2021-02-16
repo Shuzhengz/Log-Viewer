23381e1428f4337bb42934fcd26b4602948e1bf1
azarins1 <az61768@djusdstudents.org>
2021-02-15 20:58:55 -0800

    First WIP

--- src/main/java/com/team1678/logviewer/backend/Input.java
+++ src/main/java/com/team1678/logviewer/backend/Input.java
@@ -2,5 +2,4 @@ package com.team1678.logviewer.backend;
 
 import com.team1678.logviewer.*;
 public class Input {
-    //WIP
 }
--- src/main/java/com/team1678/logviewer/backend/Transfer.java
+++ src/main/java/com/team1678/logviewer/backend/Transfer.java
@@ -4,6 +4,6 @@ import com.team1678.logviewer.backend.*;
 import com.team1678.logviewer.frontend.*;
 public class Transfer {
     public void TransferToFrontend(){
-
+        //Work In Progress
     }
 }

898b6c37b3a835f25b73d00184769613431059e4
azarins1 <az61768@djusdstudents.org>
2021-02-15 20:53:00 -0800

    Added in the Transfer file WIP

--- src/main/java/com/team1678/logviewer/backend/Input.java
+++ src/main/java/com/team1678/logviewer/backend/Input.java
@@ -1,4 +1,6 @@
 package com.team1678.logviewer.backend;
 
+import com.team1678.logviewer.*;
 public class Input {
+    //WIP
 }
---
+++ src/main/java/com/team1678/logviewer/backend/Transfer.java
@@ -0,0 +1,9 @@
+package com.team1678.logviewer.backend;
+
+import com.team1678.logviewer.backend.*;
+import com.team1678.logviewer.frontend.*;
+public class Transfer {
+    public void TransferToFrontend(){
+
+    }
+}

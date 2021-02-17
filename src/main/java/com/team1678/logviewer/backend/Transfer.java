f573d14640533f95d008dba3c2155180cfd78544
azarins1 <az61768@djusdstudents.org>
2021-02-17 13:10:08 -0800

    Make basic Transfer.java with basic request method

--- src/main/java/com/team1678/logviewer/backend/Transfer.java
+++ src/main/java/com/team1678/logviewer/backend/Transfer.java
@@ -1,9 +1,16 @@
 package com.team1678.logviewer.backend;
 
+//import com.sun.tools.javac.util.StringUtils;
 import com.team1678.logviewer.backend.*;
 import com.team1678.logviewer.frontend.*;
+import java.util.*;
 public class Transfer {
-    public void TransferToFrontend(){
-        //Work In Progress
+    private Transfer(){
+        throw new IllegalStateException("Utility Class");
+    }
+    public double[] TransferToFrontend(double[] requestedData){
+        //Work In Progress Methos
+        double[] returnVal = new double[10];
+        return returnVal;
     }
 }

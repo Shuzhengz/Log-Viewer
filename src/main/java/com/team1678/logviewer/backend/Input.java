eff4cdb3c6b02be8b00b5aa89407ec95f0d7efa2
Tom <treez.zhang@gmail.com>
2021-02-20 10:14:53 -0800

    map

--- src/main/java/com/team1678/logviewer/backend/Input.java
+++ src/main/java/com/team1678/logviewer/backend/Input.java
@@ -12,12 +12,6 @@ public class Input {
         throw new IllegalStateException("Utility class");
     }
 
-    public static Map<String, List<Integer>> output(String path){
-        Map<String, List<Integer>> hm = new HashMap<String, List<Integer>>();
-        read(path);
-        return hm;
-    }
-
     public static List<List<String>> read(String path){
         List<List<String>> records = new ArrayList<>();
         try (CSVReader csvReader = new CSVReader(new FileReader(path));) {

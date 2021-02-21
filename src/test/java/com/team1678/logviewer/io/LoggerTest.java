e30120bf2b057c232d3f61cb20ff1208cb6d9970
Tom <treez.zhang@gmail.com>
2021-02-21 14:17:20 -0800

    setupTest

---
+++ src/test/java/com/team1678/logviewer/io/LoggerTest.java
@@ -0,0 +1,17 @@
+package com.team1678.logviewer.io;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoggerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void log() {
+    }
+}

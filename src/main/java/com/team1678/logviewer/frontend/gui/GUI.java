2f7a775ffae38b69e1d562ab443e29f906bc1812
indiamanansala <whywerentyoualtelfpractice08@gmail.com>
2021-02-21 10:47:44 -0800

    Adding SwingUtilities, tweaking dataset

--- src/main/java/com/team1678/logviewer/frontend/gui/GUI.java
+++ src/main/java/com/team1678/logviewer/frontend/gui/GUI.java
@@ -8,94 +8,136 @@ import java.awt.*;
 import java.awt.event.*;
 
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.ui.ApplicationFrame;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.ChartFactory;
 
+
 public class GUI implements ActionListener {
 
-    public GUI() {
+        public GUI() {
 
-        // Create frame
+            // Create frame
 
-        JFrame frame = new JFrame("Logger");
-        frame.setSize(400, 400);
-        frame.setVisible(true);
-        frame.setLayout(new GridLayout(5, 5));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            JFrame frame = new JFrame("Logger");
+            frame.setSize(400, 400);
+            frame.setVisible(true);
+            frame.setLayout(new GridLayout(5, 5));
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Create panel
-
-        JPanel panel = new JPanel();
-
-        // Create button
-
-        JButton button = new JButton("Open Graphs");
-        button.addActionListener(this);
-
-        // Adding objects to panel
-
-        panel.add(button);
-        panel.add(frame);
-
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-        String action = e.getActionCommand();
-        if (action.equals("Open Graphs")) {
-
-            // New window opens
-
-            JWindow window = new JWindow();
-            window.setSize(700, 800);
-            window.setVisible(true);
-
-            // New panel
+            // Create panel
 
             JPanel panel = new JPanel();
 
-            // Write a caption
+            // Create button
 
-            JLabel caption = new JLabel("Graphs");
+            JButton button = new JButton("Open Graphs");
+            button.addActionListener(this);
 
-            // Adding object to panel
+            // Adding objects to panel
 
-            panel.add(caption);
+            panel.add(button);
+            panel.add(frame);
+
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            String action = e.getActionCommand();
+            if (action.equals("Open Graphs")) {
+
+                // New window opens
+
+                JWindow window = new JWindow();
+                window.setSize(700, 800);
+                window.setVisible(true);
+
+                // New panel
+
+                JPanel panel = new JPanel();
+
+                // Write a caption
+
+                JLabel caption = new JLabel("Graphs");
+
+                // Creating buttons
+
+                JButton button1 = new JButton("Line 1");
+                button1.addActionListener(this);
+
+                JButton button2 = new JButton("Line 2");
+                button2.addActionListener(this);
+
+                JButton button3 = new JButton("Line 3");
+                button3.addActionListener(this);
+
+                JButton button4 = new JButton("Line 4");
+                button4.addActionListener(this);
+
+                JButton button5 = new JButton("Line 5");
+                button5.addActionListener(this);
+
+                // Adding objects to panel
+
+                panel.add(caption);
+
+                panel.add(button1);
+                panel.add(button2);
+                panel.add(button3);
+                panel.add(button4);
+                panel.add(button5);
+
+
+            }
 
         }
 
-        // Creating the line chart
+    }
+    // Creating the line chart
 
-        class Graph {
-        DefaultCategoryDataset dataset = createDataset();
+    class Graph extends ApplicationFrame {
+        public Graph(String title) {
+            super(title);
 
-        JFreeChart chart = ChartFactory.createLineChart(
-                "title", "category", "value", dataset
-            );
+            DefaultCategoryDataset dataset = createDataset();
 
-        ChartPanel panel = new ChartPanel(chart);
+            JFreeChart chart = ChartFactory.createLineChart(
+                    "title", "category", "value", createDataset(),
+                    PlotOrientation.VERTICAL, true, true, false);
+
+
+            ChartPanel panel = new ChartPanel(chart);
+            setContentPane(panel);
 
         }
+
+        // Creating the dataset
+
+        private DefaultCategoryDataset createDataset() {
+
+            DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+            // Add values and return
+
+            String data = "Data";
+
+            dataset.addValue(null, data, null);
+
+            return null;
+        }
+
+        public static void main(String[] args) {
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    createAndShowGUI();
+                }
+                // Did quite some research here, I'm still not sure how it works.
+
+                private void createAndShowGUI() {
+                }
+            });
+        }
     }
-
-    // Creating the dataset
-
-    private DefaultCategoryDataset createDataset() {
-
-        String data = "Data";
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-
-        // Add values and return
-
-        dataset.addValue(1, data, "1");
-
-        return dataset;
-    }
-
-    public static void main(String[] args) {
-
-    }
-}
-

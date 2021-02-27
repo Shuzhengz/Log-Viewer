71c22dc107c48ca182ceaa88be79796d0c55f762
AjayPenmetsa <ajaypenmetsa1210@gmail.com>
2021-02-27 11:58:47 -0800

    test

--- src/main/java/com/team1678/logviewer/frontend/gui/GUI.java
+++ src/main/java/com/team1678/logviewer/frontend/gui/GUI.java
@@ -1,24 +1,4 @@
 package com.team1678.logviewer.frontend.gui;
 
-import javax.swing.*;
-import java.awt.*;
-
 public class GUI {
-    public static void main(String args[]) {
-
-        //frame
-        JFrame frame = new JFrame("Log Viewer");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1280, 720);
-        frame.setVisible(true);
-
-
-        //top bar
-        JMenuBar mb = new JMenuBar();
-
-
-
-        //Adding Component to the frame.
-        frame.getContentPane().add(BorderLayout.NORTH, mb);
-    }
 }

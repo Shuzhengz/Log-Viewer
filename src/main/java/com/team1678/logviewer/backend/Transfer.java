6a160cc90a41b978f993e173150db097bcd68a93
azarins1 <az61768@djusdstudents.org>
2021-03-02 00:38:01 -0800

    add logs

--- src/main/java/com/team1678/logviewer/backend/Transfer.java
+++ src/main/java/com/team1678/logviewer/backend/Transfer.java
@@ -1,5 +1,8 @@
 package com.team1678.logviewer.backend;
 
+import com.team1678.logviewer.io.Logger;
+import com.team1678.logviewer.io.Severity;
+
 import java.util.*;
 
 public class Transfer {
@@ -14,8 +17,10 @@ public class Transfer {
             Processor processorVal = new Processor();
             Processor.processData();
             Map<String, String[][]> returnVal = processorVal.getProcessedData();
+            Logger.log("TransferToFrontend returns returnVal", Severity.NORMAL);
             return returnVal;
         } catch (Exception e){
+            Logger.log("Exception thrown in TransferToFrontend", Severity.ERROR);
             return null;
         }
     }

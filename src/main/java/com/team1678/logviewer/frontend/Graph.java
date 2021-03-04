70424a5237bc35957a12c04340b9eae3e3e28471
Tom <treez.zhang@gmail.com>
2021-03-03 19:06:30 -0800

    dataset class

--- src/main/java/com/team1678/logviewer/frontend/Graph.java
+++ src/main/java/com/team1678/logviewer/frontend/Graph.java
@@ -6,10 +6,7 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.ui.ApplicationFrame;
 import org.jfree.data.category.CategoryDataset;
-import org.jfree.data.xy.DefaultXYDataset;
 import org.jfree.data.xy.XYDataset;
-
-import java.awt.*;
 
 // Creating the line chart
 public class Graph extends ApplicationFrame {
@@ -17,29 +14,21 @@ public class Graph extends ApplicationFrame {
     public Graph(String title) {
         super(title);
 
-        XYDataset dataset = createDataset();
+        XYDataset dataset = Dataset.createDataset();
+
         JFreeChart chart = ChartFactory.createLineChart(
-                "title", "category", "value", (CategoryDataset) createDataset(),
+                "title", "category", "value", (CategoryDataset) Dataset.createDataset(),
                 PlotOrientation.VERTICAL, true, true, false);
 
         ChartPanel panel = new ChartPanel(chart);
         setContentPane(panel);
     }
 
-    // Creating the dataset
-    public static XYDataset createDataset() {
-        DefaultXYDataset dataset = new DefaultXYDataset();
-        // Add values and return
-        double[][] data = { {0.1, 0.2, 0.3}, {1, 2, 3} };
-        dataset.addSeries("series1", data);
-        return dataset;
-    }
-
     public static ChartPanel createGraph(){
-        XYDataset ds = createDataset();
+        XYDataset ds = Dataset.createDataset();
 
         JFreeChart chart = ChartFactory.createXYLineChart("LogViewer",
-                "x", "y", ds, PlotOrientation.VERTICAL, true, true,
+                "Time", "Value", ds, PlotOrientation.VERTICAL, true, true,
                 false);
 
         return new ChartPanel(chart);

d594fdcbe1c1a53e855a30da6bd9869ea1859042
Tom <treez.zhang@gmail.com>
2021-03-07 11:19:41 -0800

    add csv broken checker

--- src/main/java/com/team1678/logviewer/frontend/Dataset.java
+++ src/main/java/com/team1678/logviewer/frontend/Dataset.java
@@ -2,6 +2,8 @@ package com.team1678.logviewer.frontend;
 
 import com.team1678.logviewer.backend.Processor;
 
+import com.team1678.logviewer.io.Logger;
+import com.team1678.logviewer.io.Severity;
 import org.jfree.data.xy.DefaultXYDataset;
 import org.jfree.data.xy.XYDataset;
 
@@ -21,18 +23,33 @@ public class Dataset extends Graph{
     // Creating the dataset
     public static XYDataset createDataset() {
         int kHeaderCount = getHeaderCount(kHeaders);
+        boolean headerComplete = getIfHeaderComplete(kHeaderCount, getColumnCount(kOrganizedData));
+
         DefaultXYDataset dataset = new DefaultXYDataset();
 
-        for (int headerNum = 1; headerNum < kHeaderCount; headerNum++){
-            dataset.addSeries(kHeaders.get(headerNum), new double [][]
-                    {timestamp, getDataFromList(cleanOrganizedData(kOrganizedData.get(headerNum)))});
+        if (headerComplete) {
+            for (int headerNum = 1; headerNum < kHeaderCount; headerNum++) {
+                dataset.addSeries(kHeaders.get(headerNum), new double[][]
+                        {timestamp, getDataFromList(cleanOrganizedData(kOrganizedData.get(headerNum)))});
+            }
+            return dataset;
+        } else if (!headerComplete) {
+            for (int headerNum = 1; headerNum < kHeaderCount; headerNum++) {
+                try {
+                    dataset.addSeries(kHeaders.get(headerNum), new double[][]
+                            {timestamp, getDataFromList(cleanOrganizedData(kOrganizedData.get(headerNum)))});
+                    Logger.log("Broken CSV line read successful", Severity.WARNING);
+                } catch (Exception CSVBrokenException) {
+                    Logger.log("CSV file broken, read failed", Severity.ERROR);
+                }
+            }
+            return dataset;
         }
-        return dataset;
+        return null;
     }
 
     private static double [][] createCoords (int rowCount, int headerCount) {
         int totalRows = kOrganizedData.size();
-        //timestamp = getDataFromList(cleanOrganizedData(kOrganizedData.get(0)));
         List<List<String>> organizedData = kOrganizedData;
         organizedData.remove(0);
         for (int currentRowNum = rowCount; currentRowNum < totalRows; currentRowNum++) {
@@ -41,14 +58,29 @@ public class Dataset extends Graph{
         return null;
     }
 
+    private static boolean getIfHeaderComplete(int headerCount, int columnCount) {
+        if (headerCount == columnCount) {
+            Logger.log("CSV check successful", Severity.NORMAL);
+            return true;
+        } else {
+            Logger.log("CSV seems broken", Severity.WARNING);
+            return false;
+        }
+
+    }
+
     // Count how many headers there are
     private static int getHeaderCount(List<String> headerList) {
-        return headerList.size();
+        int headerSize = headerList.size();
+        Logger.log("CSV has " + headerSize + " headers", Severity.NORMAL);
+        return headerSize;
     }
 
     // Count how many columns there are
-    private static int getColumnCount(List<String> valueList) {
-        return valueList.size();
+    private static int getColumnCount(List<List<String>> valueList) {
+        int rowSize = valueList.size();
+        Logger.log("CSV has " + rowSize + " columns", Severity.NORMAL);
+        return rowSize;
     }
 
     // Get rid of the headers in organizedData
